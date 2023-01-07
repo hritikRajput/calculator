e62d803aee90f33fab60ca76efcad49094c884b0
hritikRajput <hritikshivam@gmail.com>
2023-01-08 00:19:31 +0530

    Add functionality for multiple operations in populatedisplay in javascript

--- app.js
+++ app.js
@@ -2,72 +2,89 @@ let displayValue = document.querySelector('.display-content');
 let firstNum = 0;
 let secondNum = 0;
 let operator = null;
+let result = 0;
 
-function add(num1, num2){
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1, num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1, num2){
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2){
-    return num1/num2;
+function divide(num1, num2) {
+    return Math.round(num1 / num2);
 }
 
-function operate(op, param1, param2){
-    if(op==="+"){
+function operate(op, param1, param2) {
+    if (op === "+") {
         return add(param1, param2)
     }
-    else if(op==="-"){
+    else if (op === "-") {
         return subtract(param1, param2)
     }
-    else if(op==="*"){
+    else if (op === "*") {
         return multiply(param1, param2)
     }
-    else if(op==="/"){
+    else if (op === "/") {
         return divide(param1, param2)
     }
 }
 
-function populateDisplay(e){
+function populateDisplay(e) {
     let currKey = e.target.textContent
-    if(displayValue.textContent==="0"){
-        displayValue.textContent=""
+    if (displayValue.textContent === "0") {
+        displayValue.textContent = ""
     }
-    displayValue.textContent = displayValue.textContent + currKey;
-    if(currKey==="+" || currKey==="-" || currKey==="*" || currKey==="/"){
-        operator = currKey;
-    }
-    else if(currKey==="="){
-        const result = operate(operator, firstNum, secondNum);
-        displayValue.textContent = `${result}`;
-    }
-    else{
-        if(operator===null){
-            firstNum = firstNum*10 + parseInt(currKey);
+    if (currKey === "+" || currKey === "-" || currKey === "*" || currKey === "/") {
+        if (operator !== null) {
+            const result = operate(operator, firstNum, secondNum);
+            firstNum = result;
+            secondNum = 0;
         }
-        else{
+        operator = currKey;
+        displayValue.textContent = firstNum + currKey;
+
+    }
+    else if (currKey === "=") {
+        if(!operator){
+            result =0;
+        }else{
+         result = operate(operator, firstNum, secondNum);
+        }
+        displayValue.textContent = `${result}`;
+        firstNum = result;
+        secondNum=0;
+        operator=null;
+    }
+    else {
+        displayValue.textContent = displayValue.textContent + currKey;
+
+        if (operator === null) {
+            firstNum = firstNum * 10 + parseInt(currKey);
+        }
+        else {
             secondNum = secondNum * 10 + parseInt(currKey);
         }
     }
+    console.log(firstNum, secondNum, operator, result)
 }
 
-function clearDisplay(e){
-    displayValue.textContent="0";
-    firstNum=0;
-    secondNum=0;
-    operator=null;
+function clearDisplay(e) {
+    displayValue.textContent = "0";
+    firstNum = 0;
+    secondNum = 0;
+    operator = null;
 }
 
 
-const numbers = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key');
 const clear = document.querySelector('.clear');
-numbers.forEach((number)=>{
-    number.addEventListener('click', populateDisplay)
+keys.forEach((key) => {
+    key.addEventListener('click', populateDisplay)
 });
 clear.addEventListener('click', clearDisplay)
