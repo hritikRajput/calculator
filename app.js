47466f76f96174dc66c5c7c7569845d67bd4df16
hritikRajput <hritikshivam@gmail.com>
2023-01-09 21:26:27 +0530

    Remove populate display and Add new functions for populating

--- app.js
+++ app.js
@@ -1,11 +1,66 @@
-let firstNum = 0;
-let secondNum = 0;
-let operator = null;
+let firstNum = "";
+let secondNum = "";
+let currOperand = "";
+let currOperator = null;
 let result = 0;
 
 let displayValue = document.querySelector('.display-content');
-const keys = document.querySelectorAll('.key');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 const clear = document.querySelector('.clear');
+const equal = document.querySelector('.equal');
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => populateNumber(number.textContent))
+});
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => populateOperator(operator.textContent))
+})
+clear.addEventListener('click', clearDisplay)
+equal.addEventListener('click', evaluate)
+
+
+function populateNumber(num) {
+    if (displayValue.textContent === "0") {
+        displayValue.textContent = "";
+    }
+    displayValue.textContent = displayValue.textContent + num;
+    currOperand = currOperand + num;
+}
+
+function clearDisplay() {
+    displayValue.textContent = "0";
+    firstNum = "";
+    secondNum = "";
+    currOperator = null;
+    currOperand=""
+}
+
+function populateOperator(oper) {
+    if (currOperator != null) {
+        evaluate();
+    }
+    currOperator = oper;
+    firstNum = currOperand;
+    currOperand = "";
+    displayValue.textContent = firstNum + currOperator
+}
+
+function evaluate() {
+    secondNum = currOperand;
+    if (currOperator === "/" && secondNum === "0") {
+        alert("ERROR! You can not divide by zero.");
+        clearDisplay()
+        return;
+    }
+    
+    if (firstNum && secondNum && currOperator) {
+        currOperand = `${Math.round(operate(currOperator, firstNum, secondNum) * 10) / 10}`;
+        console.log(firstNum, secondNum, currOperand, currOperator)
+        displayValue.textContent = currOperand;
+    }
+    currOperator=null;
+}
 
 // -------------- Basic Mathematical Operations -------------
 
@@ -22,12 +77,13 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return Math.round(num1 / num2);
+    return num1 / num2;
 }
 
-// 
 
 function operate(op, param1, param2) {
+    param1 = Number(param1);
+    param2 = Number(param2);
     if (op === "+") {
         return add(param1, param2)
     }
@@ -38,59 +94,13 @@ function operate(op, param1, param2) {
         return multiply(param1, param2)
     }
     else if (op === "/") {
+        if (param2 === 0) {
+            return null;
+        }
         return divide(param1, param2)
     }
-}
-
-function populateDisplay(e) {
-    let currKey = e.target.textContent
-    if (displayValue.textContent === "0") {
-        displayValue.textContent = ""
-    }
-    if (currKey === "+" || currKey === "-" || currKey === "*" || currKey === "/") {
-        if (operator !== null) {
-            const result = operate(operator, firstNum, secondNum);
-            firstNum = result;
-            secondNum = 0;
-        }
-        operator = currKey;
-        displayValue.textContent = firstNum + currKey;
-
-    }
-    else if (currKey === "=") {
-        if(!firstNum || !operator || !secondNum){
-            result =0;
-        }else{
-         result = operate(operator, firstNum, secondNum);
-        }
-        displayValue.textContent = `${result}`;
-        firstNum = result;
-        secondNum=0;
-        operator=null;
-    }
     else {
-        displayValue.textContent = displayValue.textContent + currKey;
-
-        if (operator === null) {
-            firstNum = firstNum * 10 + parseInt(currKey);
-        }
-        else {
-            secondNum = secondNum * 10 + parseInt(currKey);
-        }
+        return null;
     }
-    console.log(firstNum, secondNum, operator, result)
 }
 
-function clearDisplay(e) {
-    displayValue.textContent = "0";
-    firstNum = 0;
-    secondNum = 0;
-    operator = null;
-}
-
-
-
-keys.forEach((key) => {
-    key.addEventListener('click', populateDisplay)
-});
-clear.addEventListener('click', clearDisplay)
